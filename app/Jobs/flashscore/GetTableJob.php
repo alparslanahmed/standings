65b0ca726cc0f4845deef71164f4de775cb0c5cf
alparslanahmed <alparslanahmed@gmail.com>
2019-10-12 21:52:33 +0400

    clean the code

--- app/Jobs/flashscore/GetTableJob.php
+++ app/Jobs/flashscore/GetTableJob.php
@@ -3,7 +3,6 @@
 namespace App\Jobs\flashscore;
 
 use App\Jobs\UpdateStandingJob;
-use App\Standing;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
